61c502b5e3a8259f2758854e35c22b4f82e6aaac
Ardhan Madras <ardhan@rocksis.net>
2025-11-24 07:26:35 +0000

    Add utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::{self, Write};
+
+pub fn create_pidfile(path: &str) -> io::Result<()> {
+    let mut f = File::options()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(path)?;
+    let s = std::process::id().to_string();
+    f.write_all(s.as_bytes())
+}
